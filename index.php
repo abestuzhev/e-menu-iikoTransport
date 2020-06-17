087cd5d3a464ef8cacbc338c2d76810d52811cca
Aleksey <aleksey.bestuzhev@gmail.com>
2020-06-17 18:03:54 +0300

    render menu with modifiers

--- index.php
+++ index.php
@@ -1,25 +1,98 @@
 <? include_once "template/header.php" ?>
 
 
+<?
+// show_code("номенклатура", $_SESSION["numenclature"]["groups"]);
+// show_code("номенклатура", $_SESSION["numenclature"]["products"][0]);
+
+$menuCategory = $_SESSION["numenclature"]["groups"];
+$products = $_SESSION["numenclature"]["products"];
+
+// show_code("товары", $products);
+// show_code("товары", $products[""]);
+?>
+
 <div class="layout">
-    <h1 class="title">iikoTransport menu</h1>
+    <h1 class="title-page">iikoTransport menu</h1>
+
+    <div class="title-block">Меню категорий (groups)</div>
+    <div class="menu-product">
+        <?foreach($menuCategory as $item):?>
+            <?if($item["isIncludedInMenu"]):?>
+                <div class="menu-product__item" data-id="<?=$item["id"]?>"><?=$item["name"]?></div>
+            <?endif?>
+        <?endforeach?>
+        
+    </div>
+
+    
 
 
     <div class="product-list">
-        <div class="product-list__item">
-            <div class="product-card">
-                <div class="product-card__img">
-                    <img src="" alt="">
+        <?foreach($products as $item):?>
+            <!-- задаем пока категорию пиццы -->
+            <?if($item["parentGroup"] === "e44eb9f0-ca1d-44d0-97ed-8dde00b51ec6"):?>
+                
+                <div class="product-list__item">
+                    <div class="product-card" data-id="<?= $item["id"]?>" data-type="<?= $item["type"]?>">
+                        <div class="product-card__img">
+                            <img src="<?= $item["imageLinks"][0]?>" alt="">
+                        </div>
+                        <div class="product-card__body">
+                            <div class="product-card__title"><?= $item["name"]?></div>
+                            <div class="product-card__text"><?= $item["description"]?></div>
+                            
+                            <div class="product-card__footer">
+                                <?if(!empty($item["groupModifiers"])):?>
+                                    <div class="product-card-mode">
+                                        <?
+                                        $currentPrice = array();
+                                        ?>
+                                        <?foreach($item["groupModifiers"] as $modifier):?>
+                                            <?foreach($modifier["childModifiers"] as $modifierElement):?>                                           
+                                                <?
+                                                $modifierIdArray = array();
+                                                array_push($modifierIdArray, $modifierElement["id"]);?>
+                                                <?foreach($products as $itemModifire):?>
+                                                    <?if( $itemModifire["id"] === $modifierElement["id"]):?>
+                                                        <?
+                                                        array_push($currentPrice, $itemModifire["sizePrices"][0]["price"]["currentPrice"]);                             
+                                                        ?>
+                                                        <div class="product-card-mode__item"  
+                                                            data-modifier-id="<?=$itemModifire["id"]?>" 
+                                                            data-modifier-price="<?=$itemModifire["sizePrices"][0]["price"]["currentPrice"]?>"
+                                                        >
+                                                            <span><?=$itemModifire["name"]?></span>
+                                                        </div>
+                                                    <?endif?>
+                                                <?endforeach?>
+
+                                            
+                                                
+                                            <?endforeach?>
+                                        <?endforeach?>
+                                    </div>
+                                <?endif?>
+                                <div class="product-card__price">
+                                    Цена: 
+                                    <?
+                                    if(!empty($item["groupModifiers"])){
+                                        echo $currentPrice[0];
+                                    }else {
+                                        echo $item["sizePrices"][0]["price"]["currentPrice"];
+                                    }
+                                     
+                                     ?>
+                                     руб.
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
-                <div class="product-card__body">
-                    <div class="product-card__title"></div>
-                    <div class="product-card__text"></div>
-                </div>
-                <div class="product-card__footer">
-                    
-                </div>
-            </div>
-        </div>
+            
+            <?endif?>
+        <?endforeach?>
     </div>   
 </div>
 
--- template/header.php
+++ template/header.php
@@ -17,6 +17,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?$_SERVER['DOCUMENT_ROOT'] ?>/template/css/reset.css">
     <link rel="stylesheet" href="<?$_SERVER['DOCUMENT_ROOT'] ?>/template/css/style.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#b91d47">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta property="og:url" content="https://pizzapresto.ru">
+    <meta property="og:title" content="Быстрая доставка пиццы и роллов «Престо»"/>
+    <meta property="og:description" content="Быстрая доставка пиццы, роллов, WOK, салатов и горячих блюд!"/>
+    <meta property="og:image" content="https://pizzapresto.ru/bitrix/templates/light2/img/og-img.jpg"/>
+
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>iikoTransport</title>
 </head>
